4c71e84df6cecf1ee723511745e622b615d98790
mohammad-nili <mohammad.nili@ut.ac.ir>
2021-07-18 03:33:04 +0430

    make queries dir for cass & click

---
+++ queries/clickhouse_results.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 17 08:49:23 2021
+
+@authors: nilam & zaha
+"""
+
+from datetime import datetime, timezone
+from clickhouse_driver import Client
+from json import loads, dumps
+from datetime import date
