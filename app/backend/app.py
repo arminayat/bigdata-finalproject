89e8931b49bd61187a911319ef7bf2c7e3bf151e
arminayat <arminayat3@gmail.com>
2021-07-18 02:34:55 +0430

    webapp(backend): implement keywords endpoint to fetch keywords(hashtags) from redis

--- app/backend/app.py
+++ app/backend/app.py
@@ -8,3 +8,14 @@ import logging
 
 app = Flask(__name__)
 r = redis.Redis(host='127.0.0.1', port=6379)
+
+
+@app.route('/bd-api/keywords')
+def get_tweets_by_keyword():
+    keywords = [x.decode().split(":")[1] for x in r.keys("keyword:*")]
+    keywords_counts = {}
+    for k in keywords:
+        keywords_counts[k] = keywords.count(k)
+    resp = make_response(jsonify(keywords_counts), 200)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
