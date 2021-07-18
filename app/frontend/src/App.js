60287742da1b92b9e59a402dffd00c967800f76b
arminayat <arminayat3@gmail.com>
2021-07-18 12:07:57 +0430

    webapp(frontend): add router to app ++ add style to main page

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route, useHistory } from "react-router-dom";
+import { Layout } from "antd";
+import { Content } from "antd/lib/layout/layout";
+import Dashboard from "./Pages/Dashboard";
+import styled, { keyframes } from "styled-components";
+
+const Gradient = keyframes`
+  0% {
+    background-position: 0% 50%
+    }
+  50% {
+  background-position: 100% 50%
+  }
+  100% {
+  background-position: 0% 50%
+  }`;
+const StyledLayout = styled(Layout)`
+  background: -webkit-linear-gradient(
+    -45deg,
+    #0052b6,
+    #0080ce,
+    #1da1f2,
+    #00bef6,
+    #1da1f2,
+    #006eb9,
+    #6f94bc,
+    #0052b6
+  );
+  background: linear-gradient(
+    -45deg,
+    #0052b6,
+    #0080ce,
+    #1da1f2,
+    #00bef6,
+    #1da1f2,
+    #006eb9,
+    #6f94bc,
+    #0052b6
+  );
+  background-size: 400% 400%;
+  -webkit-animation: ${Gradient} 15s ease infinite;
+  -moz-animation: ${Gradient} 15s ease infinite;
+  animation: ${Gradient} 15s ease infinite;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <StyledLayout
+        style={{
+          height: "100%",
+          minHeight: "100vh",
+        }}
+      >
+        <Content>
+          <Switch>
+            <Route path="/">
+              {/* <Dashboard /> */}
+            </Route>
+          </Switch>
+        </Content>
+      </StyledLayout>
+    </BrowserRouter>
   );
 }
 
