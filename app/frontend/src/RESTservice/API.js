88859c8e9715b52f662695053ba455ac393f147a
root <root@162.55.30.239>
2021-07-19 19:46:02 +0430

    webapp(frontend): update frontend api for server deployment

--- app/frontend/src/RESTservice/API.js
+++ app/frontend/src/RESTservice/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const BASE_URL =
-  "http://localhost:5000/";
+  "http://162.55.30.239:5000/";
 export const API_URL = BASE_URL + "";
 
 const API = axios.create({
